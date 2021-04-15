3ee732b8f54f1cbd62fa751500fc5584b6a0ada2
leoliese <alexander_angel@web.de>
2021-04-15 15:44:07 +0200

    Update my.js

--- my.js
+++ my.js
@@ -1,9 +1,8 @@
 function testGS(){
 
 
-    const url = "https://script.google.com/macros/s/AKfycbwObAldH_BH-Nx6nSydyy3NEgj7kvkeeoBDapvqCDqKPVL4O3Jp/exec";
+    const url = "https://script.google.com/macros/s/AKfycbzTCyElVkxC4nPOAVBjc7MI6sdjc6u5Flp7zu7Rrpijwk7sdFVy7hxC6zD05BYLMSCU8g/exec";
 
-    fetch(url).then(response => response.json()).then(data => console.log(data));
 
     fetch(url)
         .then(d => d.json())
@@ -15,7 +14,35 @@ function testGS(){
 
 }
 
+function addGS(){
+
+
+    const url = "https://script.google.com/macros/s/AKfycbzTCyElVkxC4nPOAVBjc7MI6sdjc6u5Flp7zu7Rrpijwk7sdFVy7hxC6zD05BYLMSCU8g/exec";
+
+
+    fetch(url,{
+
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //credentials: 'omit', // include, *same-origin, omit
+        headers: {'Content-Type': 'application/json'
+      
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"Olivia"}) // body data type must match "Content-Type" header
+
+    });
+}
+    
+
+
+
+
 document.getElementById("btn").addEventListener("click",testGS);
+document.getElementById("btn2").addEventListener("click",addGS);
+
 
 
 
